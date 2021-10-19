da24dbf7ea7838c967accfbe16d454c1955b291b
super-sophocles <supposedly_pegasus@yahoo.co.jp>
2021-10-20 02:03:47 +0900

    first

---
+++ src/index.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react'
+import { render } from 'react-dom'
+
+import { createGlobalStyle } from 'styled-components'
+import { Editor } from './pages/editor'
+
+const GlobalStyle = createGlobalStyle`
+   body * {
+     box-sizing: border-box;
+   }
+ `
+
+const Main = (
+  <>
+    <GlobalStyle />
+    <Editor />
+  </>
+)
+
+render(Main, document.getElementById('app'))
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path')
+
+module.exports = {
+  entry: './src/index.tsx',
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.ts', '.tsx'],
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+    publicPath: 'dist/',
+  },
+  devServer: {
+   static: {
+            directory: path.join(__dirname, 'dist'),
+        },
+   hot: true,
+    open: true,
+  }
+}
